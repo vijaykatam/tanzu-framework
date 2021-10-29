9959a6224368060506bbd4da612507f53c2cc725
Vijay Katam <vkatam@vmware.com>
2021-10-28 17:05:31 -0700

    rbac updates

--- addons-apiserver/pkg/registry/addon/v1alpha1/antreaaddonconfig_rest.go
+++ addons-apiserver/pkg/registry/addon/v1alpha1/antreaaddonconfig_rest.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	metainternalversion "k8s.io/apimachinery/pkg/apis/meta/internalversion"
 	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/apiserver/pkg/endpoints/request"
 
 	"github.com/vmware-tanzu/tanzu-framework/addons-apiserver/pkg/apis/addon/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -49,6 +50,19 @@ func (r *AntreaAddonConfigREST) Get(ctx context.Context, name string, options *m
 	return &v1alpha1.AntreaAddonConfig{}, nil
 }
 func (r *AntreaAddonConfigREST) List(ctx context.Context, options *metainternalversion.ListOptions) (runtime.Object, error) {
+	secrets, err := r.client.CoreV1().Secrets(request.NamespaceValue(ctx)).List(ctx, metav1.ListOptions{LabelSelector: "tkg.tanzu.vmware.com/addon-name: antrea"})
+	if err != nil {
+		return nil, err
+	}
+	var antreaAddonConfigs []v1alpha1.AntreaAddonConfig
+	for _, s := range secrets.Items {
+		antreaAddonConfig := v1alpha1.AntreaAddonConfig{}
+		antreaAddonConfig.ObjectMeta = s.ObjectMeta
+		antreaAddonConfig.Spec.InfraProvider
+		antreaAddonConfigs = append(antreaAddonConfigs,
+			v1alpha1.AntreaAddonConfig{Name: },
+		)
+	}
 	return &v1alpha1.AntreaAddonConfigList{}, nil
 }
 func (r *AntreaAddonConfigREST) Update(ctx context.Context, name string, objInfo rest.UpdatedObjectInfo, createValidation rest.ValidateObjectFunc, updateValidation rest.ValidateObjectUpdateFunc, forceAllowCreate bool, options *metav1.UpdateOptions) (runtime.Object, bool, error) {
